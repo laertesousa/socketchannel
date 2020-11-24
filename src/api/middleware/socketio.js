c1fc96912394c7c667ac1346ce07e01d0901a636
Laerte Sousa Neto <laerte@getbento.com>
2020-11-24 18:06:32 -0500

    Track number of connections

--- src/api/middleware/socketio.js
+++ src/api/middleware/socketio.js
@@ -2,6 +2,7 @@ const io = require('socket.io');
 const roomsService = require('../components/rooms/service');
 
 let _instance = null;
+let _number_of_connections = 0;
 
 const getInstance = () => {
   return _instance;
@@ -11,17 +12,20 @@ const handleConnection = async socket => {
   socket.on('disconnect', handleDisconnect);
   const { room: roomName, accessToken } = socket.handshake.query;
   const room = await roomsService.getByName(roomName, accessToken);
-
+  _number_of_connections++;
   if (room) {
     console.log(`${socket.id} joined room ${room.name}`);
     socket.join(room.name);
   } else {
     console.warn(`${roomName} does not exist.`);
   }
+  console.log(`connections: ${_number_of_connections}`);
 };
 
 const handleDisconnect = () => {
   console.log('user disconnected');
+  _number_of_connections--;
+  console.log(`connections: ${_number_of_connections}`);
 };
 
 const init = app => {
