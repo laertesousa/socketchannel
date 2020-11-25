cad6a1091c4d2a6fc85f1f4980b42b1380e650cc
Laerte Sousa Neto <laerte@getbento.com>
2020-11-24 21:20:39 -0500

    Resolve circular dependency

--- src/api/components/rooms/service.js
+++ src/api/components/rooms/service.js
@@ -1,8 +1,6 @@
 const { Room } = require('./models');
-const eventsService = require('../../../services/events');
 
-const getRooms = async () =>
-  await Room.find().exec()
+const getRooms = async () => await Room.find().exec();
 
 const create = async (name, url, accessToken) =>
   await Room.create({ name, url, accessToken });
@@ -10,13 +8,8 @@ const create = async (name, url, accessToken) =>
 const getByName = async (name, accessToken) =>
   await Room.findOne({ name, accessToken });
 
-const sendMessage = (channel, data) => {
-  eventsService.sendMessage(channel, data);
-};
-
 module.exports = {
   getRooms,
   create,
-  getByName,
-  sendMessage
+  getByName
 };
