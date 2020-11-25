2e51abddac3da6394dc7c3fc3d8e0c0d93e22f1e
Laerte Sousa Neto <laerte@getbento.com>
2020-11-24 21:29:42 -0500

    Disable tasks on every environment but staging

--- src/tasks/event-dispatcher.js
+++ src/tasks/event-dispatcher.js
@@ -2,18 +2,22 @@ const { processEvents } = require('../services/events');
 
 const TASK_NAME = 'Handle new events';
 const setupTask = agenda => {
-  agenda.define(TASK_NAME, async (job, done) => {
-    console.log(`running ${TASK_NAME}.`);
-    try {
-      await processEvents();
-    } catch (err) {
-      console.log(`Error ${TASK_NAME} - ${err.message}`);
-    } finally {
-      console.log(`completed ${TASK_NAME}.`);
-    }
+  agenda.define(
+    TASK_NAME,
+    { priority: 'high', concurrency: 1 },
+    async (job, done) => {
+      console.log(`running ${TASK_NAME}.`);
+      try {
+        await processEvents();
+      } catch (err) {
+        console.log(`Error ${TASK_NAME} - ${err.message}`);
+      } finally {
+        console.log(`completed ${TASK_NAME}.`);
+      }
 
-    done();
-  });
+      done();
+    }
+  );
 
   return TASK_NAME;
 };
--- src/tasks/index.js
+++ src/tasks/index.js
@@ -4,8 +4,13 @@ const eventDispatcherTask = require('./event-dispatcher');
 const dbBaseUrl = process.env.DB_URL;
 const username = process.env.DB_USER;
 const password = process.env.DB_PASS;
+const enable_tasks = process.env.ENABLE_TASKS;
 
 const setupTask = async () => {
+  if (!enable_tasks) {
+    return;
+  }
+
   const agenda = new Agenda({
     db: { address: `mongodb://${username}:${password}@${dbBaseUrl}` }
   });
