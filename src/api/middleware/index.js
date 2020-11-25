2f72397a6aa9594d7e47b97c42c51f8cb8435cd3
Aditi Mocharla <aditi_mocharla@Aditis-MacBook-Pro.local>
2020-11-25 13:04:12 -0500

    Adding example env

--- src/api/middleware/index.js
+++ src/api/middleware/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-
 const setupDatabase = require('./database');
 const { init } = require('./socketio');
 
